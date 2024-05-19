ac5002a47ebd7d0ac90bdbdd8c1e486bba0a762e
freddygaffey <149978677+freddygaffey@users.noreply.github.com>
2024-05-19 22:13:59 +1000

    add loop for testing

---
+++ code/implimant matrix in cpp in esp/wifi.cpp
@@ -0,0 +1,14 @@
+#include <HardwareSerial.h>
+void setup() {
+  Serial.begin(115200);
+  Serial.println("hello world");
+  // put your setup code here, to run once:
+}
+
+void loop() {
+ 
+    Serial.println("connected...yeey :)");
+    
+
+  // put your main code here, to run repeatedly:
+}
